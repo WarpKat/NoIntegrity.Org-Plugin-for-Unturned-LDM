878e4bc7ce5aed2ac87e928105e25576d9dbac02
WarpKat <warpkat@nointegrity.org>
2024-02-23 11:57:28 -0800

    Fire-and-forget the Discord messages when a player joins or leaves.

--- Functions/DiscordMsg.cs
+++ Functions/DiscordMsg.cs
@@ -4,6 +4,7 @@ using Steamworks;
 using System;
 using System.IO;
 using System.Net;
+using System.Threading;
 
 namespace NoIntegrity.Functions
 {
@@ -11,37 +12,43 @@ namespace NoIntegrity.Functions
     {
         public static void OnPlayerConnected_DMJoin(UnturnedPlayer player)
         {
-            NoIntegrityConfiguration config = NoIntegrity.Instance.Configuration.Instance;
+                NoIntegrityConfiguration config = NoIntegrity.Instance.Configuration.Instance;
 
-            string thisWebhook = config.discordPlayerConnectWebhook;
+                string thisWebhook = config.discordPlayerConnectWebhook;
 
-            string thisPlayer = player.CharacterName;
-            //string thisServerIP = SteamGameServer.GetPublicIP().ToString();
-            //string thisServerPort = Provider.port.ToString();
-            string thisServerName = Provider.serverName.ToString();
-            string thisServerMap = Provider.map.ToString();
-            string thisServerID = SteamGameServer.GetSteamID().ToString();
-            string thisBotName = config.discordPlayerStatesBotname;
-            string thisMessage = $"{thisPlayer} has joined {thisServerName} - {thisServerMap}\\nConnect directly using this server code:  {thisServerID}";
-            string thisContent = "{\"username\":\"" + thisBotName + "\",\"content\":\"" + thisMessage + "\"}";
+                string thisPlayer = player.CharacterName;
+                string thisServerName = Provider.serverName.ToString();
+                string thisServerMap = Provider.map.ToString();
+                string thisServerID = SteamGameServer.GetSteamID().ToString();
+                string thisBotName = config.discordPlayerStatesBotname;
+                string thisMessage = $"{thisPlayer} has joined {thisServerName} - {thisServerMap}\\nConnect directly using this server code:  {thisServerID}";
+                string thisContent = "{\"username\":\"" + thisBotName + "\",\"content\":\"" + thisMessage + "\"}";
 
-            sendDiscordWebhook(thisWebhook, thisContent);
+            Thread thread = new Thread(() =>
+            {
+                sendDiscordWebhook(thisWebhook, thisContent);
+            });
+            thread.Start();
         }
 
         public static void OnPlayerDisconnected_DMLeft(UnturnedPlayer player)
         {
-            NoIntegrityConfiguration config = NoIntegrity.Instance.Configuration.Instance;
+                NoIntegrityConfiguration config = NoIntegrity.Instance.Configuration.Instance;
 
-            string thisWebhook = config.discordPlayerDisconnectWebhook;
+                string thisWebhook = config.discordPlayerDisconnectWebhook;
 
-            string thisPlayer = player.CharacterName;
-            string thisServerName = Provider.serverName.ToString();
-            string thisBotName = config.discordPlayerStatesBotname;
-            string thisServerMap = Provider.map.ToString();
-            string thisMessage = $"{thisPlayer} has left {thisServerName} - {thisServerMap}";
-            string thisContent = "{\"username\":\"" + thisBotName + "\",\"content\":\"" + thisMessage + "\"}";
+                string thisPlayer = player.CharacterName;
+                string thisServerName = Provider.serverName.ToString();
+                string thisBotName = config.discordPlayerStatesBotname;
+                string thisServerMap = Provider.map.ToString();
+                string thisMessage = $"{thisPlayer} has left {thisServerName} - {thisServerMap}";
+                string thisContent = "{\"username\":\"" + thisBotName + "\",\"content\":\"" + thisMessage + "\"}";
 
-            sendDiscordWebhook(thisWebhook, thisContent);
+            Thread thread = new Thread(() =>
+            {
+                sendDiscordWebhook(thisWebhook, thisContent);
+            });
+            thread.Start();
         }
 
 
